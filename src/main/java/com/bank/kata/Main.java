e1e8cdf451faadb9273c65086f3c3a51f72ce748
rami510 <rami.bouabid@gmail.com>
2024-11-20 21:26:38 +0100

    Test statement printing in the main console

--- src/main/java/com/bank/kata/Main.java
+++ src/main/java/com/bank/kata/Main.java
@@ -2,5 +2,14 @@ package com.bank.kata;
 
 public class Main {
     public static void main(String[] args) {
+        Account account = new Account();
+
+        // Dépôts et retraits
+        account.deposit(300);
+        account.withdraw(100);
+
+        // Impression du relevé
+        System.out.println("Account Statement:");
+        account.printStatement();
     }
 }
